4696e0be2171553ce5238365bd6316153e1f1b1e
HafizAbdullah7 <hafizab200@gmail.com>
2023-10-15 12:32:51 -0700

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/download', (req, res) => {
     .pipe(res);
 });
 
-
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+const PORT =process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} `);
 });
