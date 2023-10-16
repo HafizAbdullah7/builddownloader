a2a4d4466c810a52f0cf454647c31b0c390c29fe
HafizAbdullah7 <hafizab200@gmail.com>
2023-10-16 05:12:34 -0700

    first commit

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(cors());
+
 
 app.get('/download', (req, res) => {
   const url = req.query.url;
   ytdl(url, { filter: format => format.container === 'mp4' })
     .pipe(res);
 });
-
+app.use(express.static(path.join(__dirname, "./build")));
 const PORT =process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} `);
